4e8b26243acf9e7673691e6e9861d63cc1b52bbc
Albytrosss <99574693+Albytrosss@users.noreply.github.com>
2024-07-18 01:22:59 +1000

    Week 4 Requirements + Challenge

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,23 +1,61 @@
+using System.Collections;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public int targetHP = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void Hit(int dmg)
-    {
-        targetHP -= dmg;
+    private int myHP = 1;
+    private int mySpeed = 10;
+    private Vector3 direction = Vector3.zero;
+    public TargetSize targetSize;
 
-        Debug.Log("DMG Dealt: " + dmg);
-        if (targetHP <= 0)
-        {
-            DestroyTarget();
-            Debug.Log("Target Destroyed");
-        }
+    private void Update()
+    {
+        transform.Translate(direction * mySpeed * Time.deltaTime);
     }
 
-    void DestroyTarget()
+    public void Setup()
     {
-        Destroy(gameObject);
+        StartCoroutine(Move(true));
+        
+        switch (targetSize)
+        {
+            case TargetSize.Small:
+                Resize(0.5f);
+                myHP = 10;
+                mySpeed = 1;
+                GetComponent<Renderer>().material.color = Color.green;
+                return;
+            case TargetSize.Medium:
+                Resize(1f);
+                myHP = 2;
+                mySpeed = 2;
+                GetComponent<Renderer>().material.color = Color.yellow;
+                return;
+            case TargetSize.Large:
+                Resize(1.5f);
+                myHP = 3;
+                mySpeed = 3;
+                GetComponent<Renderer>().material.color = Color.red;
+                return;
+        }
+
+    }
+    public void Hit(int dmg)
+    {
+        myHP -= dmg;
+        if (myHP <= 0)
+            DestroyTarget();
+    }
+    void DestroyTarget() => Destroy(gameObject);
+    void Resize(float _scale) => transform.localScale = new Vector3(_scale, (_scale * 0.01f), _scale);
+
+    private IEnumerator Move(bool _moveLeft)
+    {
+        Debug.Log("Coroutine Started Successfully");
+        direction = _moveLeft ? Vector3.left : Vector3.right;
+        Debug.Log("isMoving");
+        yield return new WaitForSeconds(3);
+        Debug.Log("ChangeDirection");
+        StartCoroutine(Move(!_moveLeft));
     }
 }
--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -2,32 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TargetSize { Small, Medium, Large, }
 public class TargetManager : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject[] targetTypes;
+    
+    
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
             spawnRandomTarget();
-            Debug.Log("Input: 'I'");
         }
     }
 
     void spawnRandomTarget()
     {
+
         int rndSpwn = Random.Range(0, spawnPoints.Length);
         int rndTrgt = Random.Range(0, targetTypes.Length);
         spawnTarget(targetTypes[rndTrgt], spawnPoints[rndSpwn].transform);
-        Debug.Log("RandomTarget initiated");
     }
 
     void spawnTarget(GameObject _target, Transform _spawnPoint)
     {
         GameObject target = Instantiate(_target, _spawnPoint);
-        Debug.Log("Target:"+ _target);
-        Debug.Log("Spawn:"+_spawnPoint);
+        target.GetComponent<Target>().Setup();
     }
 }
